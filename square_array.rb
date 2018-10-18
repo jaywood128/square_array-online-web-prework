ef80acabfe998d9814aaedd661c1f779a1ba9696
Johnathon Wood <jaywood128@gmail.com>
2018-10-18 21:39:36 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
  empty_array = []
   array.each do |num|
     empty_array.push(num ** 2)
+      return empty_array 
   end
     # your code here
 end
 
-square_array(array)
+
